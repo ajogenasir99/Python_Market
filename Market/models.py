c9bdfc2a6bbe9e77f8ae123e0ed84fc4bc6dffdc
Nas <nazylamar9@gmail.com>
2022-05-15 18:41:58 +0100

    implemented registration page

--- Market/models.py
+++ Market/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from enum import unique
 from Market import db
 
 class User(db.Model):
--- Market/routes.py
+++ Market/routes.py
@@ -1,6 +1,9 @@
+import imp
 from Market import app
 from flask import render_template
 from Market.models import Item
+from Market.forms import RegisterForm
+
 @app.route("/")
 @app.route("/home")
 def home_page():
@@ -11,3 +14,8 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
